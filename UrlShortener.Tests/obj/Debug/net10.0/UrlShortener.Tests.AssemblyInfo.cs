58f0b2c097ca7e04290a037ac740609904d9407f
AnLVcore2709 <anlvcore2709@gmail.com>
2026-04-12 08:49:38 +0700

    update develop

--- UrlShortener.Tests/obj/Debug/net10.0/UrlShortener.Tests.AssemblyInfo.cs
+++ UrlShortener.Tests/obj/Debug/net10.0/UrlShortener.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee3cadbc4bf5c12d06d79f18eb7ec4976758899")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
